f5f0b07281f389feb0c6269ae2b718be781a1c9e
Integr-0 <erik.reitbauer@outlook.de>
2023-11-29 21:03:30 +0100

    MultiThreadingService and Formatting

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -2,6 +2,7 @@ package utils
 
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import kotlin.io.path.Path
@@ -23,50 +24,50 @@ fun Any?.println() = println(this)
  * Hash String into **MD2**
  * @return the Hashed string
  */
-fun String.md2() = BigInteger(1, MessageDigest.getInstance("MD2").digest(toByteArray())).toString(16)
+fun String.md2(): String = BigInteger(1, MessageDigest.getInstance("MD2").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **MD5**
  * @return the Hashed string
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **SHA1**
  * @return the Hashed string
  */
-fun String.sha1() = BigInteger(1, MessageDigest.getInstance("SHA-1").digest(toByteArray())).toString(16)
+fun String.sha1(): String = BigInteger(1, MessageDigest.getInstance("SHA-1").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **SHA224**
  * @return the Hashed string
  */
-fun String.sha224() = BigInteger(1, MessageDigest.getInstance("SHA-224").digest(toByteArray())).toString(16)
+fun String.sha224(): String = BigInteger(1, MessageDigest.getInstance("SHA-224").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **SHA256**
  * @return the Hashed string
  */
-fun String.sha256() = BigInteger(1, MessageDigest.getInstance("SHA-256").digest(toByteArray())).toString(16)
+fun String.sha256(): String = BigInteger(1, MessageDigest.getInstance("SHA-256").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **SHA384**
  * @return the Hashed string
  */
-fun String.sha384() = BigInteger(1, MessageDigest.getInstance("SHA-384").digest(toByteArray())).toString(16)
+fun String.sha384(): String = BigInteger(1, MessageDigest.getInstance("SHA-384").digest(toByteArray())).toString(16)
 
 /**
  * Hash String into **SHA512**
  * @return the Hashed string
  */
-fun String.sha512() = BigInteger(1, MessageDigest.getInstance("SHA-512").digest(toByteArray())).toString(16)
+fun String.sha512(): String = BigInteger(1, MessageDigest.getInstance("SHA-512").digest(toByteArray())).toString(16)
 
 /**
  * Class for easier **Multithreading**
  * @param threads amount of threads to use for execution
  */
 class MultiThreadingService(threads: Int) {
-    val service = Executors.newFixedThreadPool(threads)
+    private val service: ExecutorService = Executors.newFixedThreadPool(threads)
 
     /**
      * Executes the given **Runnable** in one of the **threads specified during the creation**
