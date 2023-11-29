a58a9dfbf2bbb79d188e9f2f5891fca76f7cde77
Integr-0 <erik.reitbauer@outlook.de>
2023-11-29 21:00:28 +0100

    MultiThreadingService and Formatting

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,5 +7,18 @@ import utils.*
  * **See Also:** [Template](https://github.com/kotlin-hands-on/advent-of-code-kotlin-template)
  */
 fun main() {
+    val srvc = MultiThreadingService(2)
 
+    srvc.exec {
+        Thread.sleep(10);
+        println("A")
+    }
+
+    srvc.exec {
+        println("B")
+        Thread.sleep(10000)
+    }
+
+    srvc.await(10)
+    println("DONE")
 }
--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -2,6 +2,8 @@ package utils
 
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
@@ -18,36 +20,75 @@ fun readInput(name: String) = Path("src/inputs/$name.txt").readLines()
 fun Any?.println() = println(this)
 
 /**
- * Hash String into MD2
+ * Hash String into **MD2**
+ * @return the Hashed string
  */
 fun String.md2() = BigInteger(1, MessageDigest.getInstance("MD2").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into MD5
+ * Hash String into **MD5**
+ * @return the Hashed string
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into SHA1
+ * Hash String into **SHA1**
+ * @return the Hashed string
  */
 fun String.sha1() = BigInteger(1, MessageDigest.getInstance("SHA-1").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into SHA224
+ * Hash String into **SHA224**
+ * @return the Hashed string
  */
 fun String.sha224() = BigInteger(1, MessageDigest.getInstance("SHA-224").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into SHA256
+ * Hash String into **SHA256**
+ * @return the Hashed string
  */
 fun String.sha256() = BigInteger(1, MessageDigest.getInstance("SHA-256").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into SHA384
+ * Hash String into **SHA384**
+ * @return the Hashed string
  */
 fun String.sha384() = BigInteger(1, MessageDigest.getInstance("SHA-384").digest(toByteArray())).toString(16)
 
 /**
- * Hash String into SHA512
+ * Hash String into **SHA512**
+ * @return the Hashed string
  */
 fun String.sha512() = BigInteger(1, MessageDigest.getInstance("SHA-512").digest(toByteArray())).toString(16)
+
+/**
+ * Class for easier **Multithreading**
+ * @param threads amount of threads to use for execution
+ */
+class MultiThreadingService(threads: Int) {
+    val service = Executors.newFixedThreadPool(threads)
+
+    /**
+     * Executes the given **Runnable** in one of the **threads specified during the creation**
+     * @param runnable the Runnable to execute
+     */
+    fun exec(runnable: Runnable) {
+        service.execute(runnable)
+    }
+
+    /**
+     * Waits for all threads to **finish** before **shutting down**
+     * @param maxWait the maximum time to wait
+     */
+    fun await(maxWait: Int) {
+        service.shutdown()
+        service.awaitTermination(maxWait.toLong(), TimeUnit.SECONDS)
+    }
+
+    /**
+     * Forces all threads to **stop execution immediately**
+     */
+    fun forceShutdown() {
+        service.shutdownNow()
+    }
+}
